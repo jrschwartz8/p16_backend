c2c7922eae7da487a03b3918a7509b3421d8ca5d
Jason Schwartz <jrschwartz8@wisc.edu>
2025-11-19 12:49:35 -0600

    Cleaned up backend repo

--- src/components/Contact.jsx
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-    // May have to check over Heading rules
-    return <div>
-        <h1>For More Information, Please Contact:</h1>
-            <div><strong>Ayaan Nihal</strong> at <em>anihal@wisc.edu</em></div>
-            <div><strong>Gabino Ocotl</strong> at <em>gocotl@wisc.edu</em></div>
-            <div><strong>Jason Schwartz</strong> at <em>jrschwartz8@wisc.edu</em></div>
-    </div>
-}
